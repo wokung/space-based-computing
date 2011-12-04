2c2951fe3f273c5f7c8cefbab468f9aeb270e61c
Philipp Raich <philipp.raich@gmail.com>
2011-12-04 21:40:02 +0100

    HERE ERROR with producer -> gui

--- spacebsp1/src/main/java/tu/spacebased/bsp1/workers/Producer.java
+++ spacebsp1/src/main/java/tu/spacebased/bsp1/workers/Producer.java
@@ -138,7 +138,7 @@ public class Producer implements Runnable {
 						        break;
 			}
 			
-			Entry id = new Entry(newComponent, LabelCoordinator.newCoordinationData(component.toString()));
+			Entry id = new Entry(newComponent, LabelCoordinator.newCoordinationData("CPU"));
 			
 			try {
 				capi.write(cRef, RequestTimeout.INFINITE, null, id);
